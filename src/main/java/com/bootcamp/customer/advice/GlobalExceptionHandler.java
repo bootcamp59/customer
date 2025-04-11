5bedda68e3099b39394c0f4341f6f99462d96d36
devRoberth <irazabarob44@gmail.com>
2025-04-11 01:16:19 -0500

    reporte

---
+++ src/main/java/com/bootcamp/customer/advice/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.bootcamp.customer.advice;
+
+
+import com.bootcamp.customer.error.AccountErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<AccountErrorResponse> handleIllegalArgument(IllegalArgumentException ex, WebRequest request) {
+        AccountErrorResponse errorResponse = AccountErrorResponse.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.UNPROCESSABLE_ENTITY.value())
+                .error(HttpStatus.UNPROCESSABLE_ENTITY.getReasonPhrase())
+                .message(ex.getMessage())
+                .path(request.getDescription(false).replace("uri=", ""))
+                .build();
+
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(errorResponse);
+    }
+}
